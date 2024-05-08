663ac47fd178ec99f8b7c9bc98324f66ec23148b
NiamNjam <nida.kirdulyte@gmail.com>
2024-05-08 20:08:35 +0300

    carDrift

--- Assets/Scripts/Vehicle.cs
+++ Assets/Scripts/Vehicle.cs
@@ -4,28 +4,58 @@ using UnityEngine;
 
 public class Vehicle : MonoBehaviour
 {
-    public float speed = 10f;
+    public float maxSpeed = 10f;
+    public float maxReverseSpeed = -5f;
+    public float acceleration = 5f;
+    public float brakeAcceleration = -3f;
     public float turnSpeed = 10f;
     public AudioSource engineSound;
     public Rigidbody rb;
+    public AnimationCurve pitchCurve;
+    Vector3 localVelocity;
+    public TrailRenderer tr;
+    public TrailRenderer tr1;
+    public AudioSource source;
 
     void Update()
     {
         print(rb.velocity.magnitude * 3.6f);
-        engineSound.pitch = 1 + rb.velocity.magnitude * 0.1f;
+        var speedRatio = rb.velocity.magnitude / maxSpeed;
+        engineSound.pitch = pitchCurve.Evaluate(speedRatio);
+        localVelocity = transform.InverseTransformVector(rb.velocity); //drifting
+        print(localVelocity);
+        if (localVelocity.x < -1 || localVelocity.x > 1)
+        {
+            source.volume = 1;
+            print("drift");
+            tr.widthMultiplier = 0.5f;
+            tr1.widthMultiplier = 0.5f;
+            
+        }
+        else
+        {
+            source.volume = 0;
+            tr.widthMultiplier = 0.0f;
+            tr1.widthMultiplier = 0.0f;
+        }
+
+        rb.velocity += -transform.forward * localVelocity.z * 0.1f * Time.deltaTime;
+        rb.velocity += -transform.forward * localVelocity.z * 1 * Time.deltaTime;
     }
     public void Gas()
     {
-        rb.velocity += transform.forward * speed * Time.deltaTime;
+        if (localVelocity.z > maxSpeed) return;
+        rb.velocity += transform.forward * acceleration * Time.deltaTime;
         
     }
     public void Brake()
     {
-
+        if (localVelocity.z < maxReverseSpeed) return;
+        rb.velocity += transform.forward * brakeAcceleration * Time.deltaTime;
     }
     public void Turn(float amount)
     {
-        transform.Rotate(0, amount * turnSpeed, 0);
+        transform.Rotate(0, amount * turnSpeed * Time.deltaTime, 0);
     }
 
 }
