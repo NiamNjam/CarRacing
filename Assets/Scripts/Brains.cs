53ac4545a51c9ef1ff529d9d59bb6402ae11c94f
NiamNjam <146223019+NiamNjam@users.noreply.github.com>
2024-05-12 00:28:31 +0300

    AI

--- Assets/Scripts/Brains.cs
+++ Assets/Scripts/Brains.cs
@@ -5,14 +5,31 @@ using UnityEngine;
 public class Brains : MonoBehaviour
 {
     Vehicle vehicle;
+    public Transform target;
+    Path path;
 
     void Start()
     {
         vehicle = GetComponent<Vehicle>();
+        path = FindObjectOfType<Path>();
+        target = path.GetClosestPoint(transform.position);
     }
     void Update()
     {
+        float distanceToTarget = Vector3.Distance(transform.position, target.position);
+        if (distanceToTarget < 3)
+        {
+            target = path.GetNextPoint(transform.position);
+        }
+        Debug.DrawLine(transform.position, target.position, Color.red);
+        Vector3 targetDir = target.position - transform.position;
+        targetDir.Normalize();
+        float angle = Vector3.SignedAngle(transform.forward, targetDir, Vector3.up);
         vehicle.Gas();
-        vehicle.Turn(Random.Range(-1f, 1f));
+        if(angle > 3 || angle < -3)
+        {
+            vehicle.Turn(angle);
+        }
+        
     }
 }
